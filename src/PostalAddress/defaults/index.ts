d674bea66d6cfe62b2679bf4d01beaf24490a397
Jori Lethinen <lehtinenjori03@gmail.com>
2026-04-13 14:10:00 +0300

    chore: initial PostalAddress class

---
+++ src/PostalAddress/defaults/index.ts
@@ -0,0 +1,49 @@
+import type { PostalAddress } from 'schema-dts'
+import type { ISO31661Alpha2 } from '@sovereignbase/utils'
+
+type SchemaOrgPostalAddressRaw = Extract<
+  PostalAddress,
+  { '@type': 'PostalAddress' }
+>
+
+export type SchemaOrgPostalAddress = Partial<
+  Record<
+    keyof SchemaOrgPostalAddressRaw,
+    SchemaOrgPostalAddressRaw[keyof SchemaOrgPostalAddressRaw]
+  >
+>
+
+export const defaults = {
+  additionalType: '',
+  addressCountry: '' as ISO31661Alpha2,
+  addressLocality: '',
+  addressRegion: '',
+  alternateName: '',
+  areaServed: '',
+  availableLanguage: '',
+  contactOption: '',
+  contactType: '',
+  description: '',
+  disambiguatingDescription: '',
+  email: '',
+  extendedAddress: '',
+  faxNumber: '',
+  hoursAvailable: '',
+  identifier: '',
+  image: '',
+  mainEntityOfPage: '',
+  name: '',
+  owner: '',
+  postOfficeBoxNumber: '',
+  postalCode: '',
+  potentialAction: '',
+  productSupported: '',
+  sameAs: '',
+  streetAddress: '',
+  subjectOf: '',
+  telephone: '',
+  url: '',
+} as const
+
+type MissingKeys = Exclude<keyof SchemaOrgPostalAddress, keyof typeof defaults>
+type ExtraKeys = Exclude<keyof typeof defaults, keyof SchemaOrgPostalAddress>
